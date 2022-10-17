fac1d024e7c3e96340ed315c6fb1e9c8564c94b0
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-10-16 22:23:03 -0500

    funcional

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,35 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
-
+import re
 def ingest_data():
+  with open('clusters_report.txt') as report:
+    filas = report.readlines()
+  filas = filas[4:]
+  #arreglo de los arreglos que van a conformar el dataframe
+  clusters = []
+  #orden del cluster 
+  #numero, valor, porcentaje y el texto.
+  cluster = [0, 0, 0, '']
+  for fila in filas:
+    if re.match('^ +[0-9]+ +', fila):
+      numero, valor, porcentaje, *texto = fila.split()
+      cluster[0] = int(numero)
+      cluster[1] = int(valor)
+      cluster[2] = float(porcentaje.replace(',','.'))
+      texto.pop(0) 
+      texto = ' '.join(texto)
+      cluster[3] += texto
 
-    #
-    # Inserte su código aquí
-    #
+    elif re.match('^\n', fila) or re.match('^ +$', fila):
+      cluster[3] = cluster[3].replace('.', '') 
+      clusters.append(cluster)
+      cluster = [0, 0, 0, '']
 
-    return df
+    elif re.match('^ +[a-z]', fila):
+      texto = fila.split()
+      texto = ' '.join(texto)
+      cluster[3] += ' ' + texto
+  df = pd.DataFrame (clusters, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+  #print(clusters)
+  return df
